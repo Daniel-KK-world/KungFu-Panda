ddee901d93a113153dd84a550a9c4153371bc02a
Daniel P.K <danilofletcher978@gmail.com>
2025-04-13 22:27:33 +0000

    OOP setup

--- kung_fu_panda.py
+++ kung_fu_panda.py
@@ -2,12 +2,21 @@ import pygame
 
 pygame.init()
 
-#display setup 
-screen = pygame.display.set_mode((800, 600))
-pygame.display.set_caption("Kung Fu Panda")
-
-#clock for fps
+# Font that is used to render the text
+font20 = pygame.font.Font('freesansbold.ttf', 20)
+ 
+# RGB values of standard colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+ 
+# Basic parameters of the screen
+WIDTH, HEIGHT = 900, 600
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Pong")
+# Used to adjust the frame rate
 clock = pygame.time.Clock()
+FPS = 30
 
 #main game loop
 running = True 
