ece2f76274ff9902d8e710387ae1a00513fc9d5e
Daniel P.K <danilofletcher978@gmail.com>
2025-04-15 11:30:43 +0000

    background

--- kung_fu_panda.py
+++ kung_fu_panda.py
@@ -4,6 +4,10 @@ pygame.init()
 
 # Font that is used to render the text
 font20 = pygame.font.Font('freesansbold.ttf', 20)
+
+#setting a background
+background_img = pygame.image.load('bg\dfqdssy-e24be69d-bdc3-4c49-8dc6-a1675168f19e.png')
+background_img = pygame.transform.scale(background_img, (900, 600))  # Scale to screen size
  
 # RGB values of standard colors
 BLACK = (0, 0, 0)
@@ -11,46 +15,48 @@ WHITE = (255, 255, 255)
 GREEN = (0, 255, 0)
  
 # Basic parameters of the screen
-WIDTH, HEIGHT = 900, 600
+WIDTH, HEIGHT = 700, 500
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Pong")
 # Used to adjust the frame rate
 clock = pygame.time.Clock()
 FPS = 30
 
-#Descender Class 
-class Drop():
-    def __init__(self):
-        pass
-    
-#Assecnder Class (Po)
-class Ascend():
-    def __init__(self):
-        pass 
-    
-class Power_ups():
-    def __init__(self):
-        pass
-    
-    
+#my sticks 
+class Sticks:
+    def __init__(self, x, y, width=20, height=600):
+        self.rect = pygame.Rect(x, y, width, height)
+        self.color = (128, 128, 0) # brown 
+        
+    def draw(self, screen):
+        pygame.draw.rect(screen, self.color, self.rect)
 
+sticks = [
+    Sticks(100, 0),
+    Sticks(200, 0),  # y is 0 to start at top
+    Sticks(500, 0),
+    Sticks(600, 0)
+]
 
-#main game loop
+# Main game loop
 running = True 
 while running:
-    #Handle events 
+    # Handle events 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False 
 
-    #To well update game objects 
+    # Draw background first
+    screen.blit(background_img, (0, 0))
+    
+    # Draw all sticks
+    for stick in sticks:
+        stick.draw(screen)
 
-    #Draw my stuff like the sticks blah blah 
-    screen.fill((0, 0, 0))
+    # Update display
     pygame.display.flip()
     
-    #stable frame rate
-    clock.tick(60) 
+    # Stable frame rate
+    clock.tick(FPS)  # Use the FPS constant you defined
     
-#now ideally we'd quit too 
 pygame.quit()
